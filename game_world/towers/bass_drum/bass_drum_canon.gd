f9b0b691e33e38f160fa1fae002e46a71d812d80
Elscrux <nickposer2102@gmail.com>
2025-10-14 16:44:52 +0200

    Move around towers

--- game_world/towers/bass_drum/bass_drum_canon.gd
+++ game_world/towers/bass_drum/bass_drum_canon.gd
@@ -3,6 +3,8 @@ extends Tower
 @onready var projectile = preload("res://game_world/towers/projectiles/projectile.tscn")
 @onready var projectile_start = $ProjectileStart
 
+@export var projectile_speed: float = 15
+
 func _process(_delta):
 	super._process(_delta)
 	turn_to_closest_enemy()
@@ -22,4 +24,4 @@ func fire():
 	node.damage = damage
 	node.global_position = projectile_start.global_position
 	var target_vector = global_position.direction_to(target_enemy.global_position)
-	node.apply_central_impulse(target_vector * 15)
+	node.apply_central_impulse(target_vector * projectile_speed)
