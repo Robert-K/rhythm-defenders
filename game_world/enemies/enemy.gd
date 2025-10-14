503af53a8df4365caac7a20c0e3018a152ba2fdb
Elscrux <nickposer2102@gmail.com>
2025-10-14 11:11:44 +0200

    Setup demo map 1

--- game_world/enemies/enemy.gd
+++ game_world/enemies/enemy.gd
@@ -3,7 +3,15 @@ class_name Enemy
 
 @export var health: int = 100
 
-@export var speed: float = 100
+@export var speed: float = 1
 
 func _ready() -> void:
-	create_tween().tween_property(self, "progress_ratio", 1, 1000 / speed)
+	create_tween().tween_property(self, "progress_ratio", 1, 20 * speed)
+
+func hit(projectile: Projectile) -> void:
+	queue_free()
+
+func _on_enemy_body_entered(body: Node3D) -> void:
+	print("entered", body)
+	var projectile = (body as Projectile)
+	hit(projectile)
