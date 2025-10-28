498d0c74b31e410955edd7b6f1f47f42c03f3ac0
Elscrux <nickposer2102@gmail.com>
2025-10-28 23:19:09 +0100

    Add vibration when the castle is hit

--- game_world/speaker_castle.gd
+++ game_world/speaker_castle.gd
@@ -10,12 +10,14 @@ const HARDCODED_BPM = 75.0
 @export var total_health: float = 100
 var health: float = total_health
 
+@export var vibration_duration_seconds: float = 1
+
 func _ready() -> void:
 	timer.wait_time = 60/HARDCODED_BPM / MusicPlayer.pitch_scale
 	timer.timeout.connect(_on_timeout)
 	update_health_label()
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	timer.wait_time = 60/HARDCODED_BPM / MusicPlayer.pitch_scale
 
 func _on_timeout() -> void:
@@ -24,7 +26,15 @@ func _on_timeout() -> void:
 
 func deal_damage(damage: float):
 	health -= damage
+	vibration()
 	update_health_label()
+
+func vibration():
+	var strength = (total_health - health) / total_health
+	print(strength)
+	Input.vibrate_handheld((int) (1000 * vibration_duration_seconds), strength)
+	for joypad_id in Input.get_connected_joypads():
+		Input.start_joy_vibration(joypad_id, 1, strength, vibration_duration_seconds)
 
 func update_health_label():
 	health_label.text = str(health).pad_decimals(0)
