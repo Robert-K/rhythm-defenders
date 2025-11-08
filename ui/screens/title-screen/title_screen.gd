2d77d8ad3f549ee6f28101d2b4fa9417cbff553a
Elscrux <nickposer2102@gmail.com>
2025-11-08 12:22:46 +0100

    Skip intro via keypress

--- ui/screens/title-screen/title_screen.gd
+++ ui/screens/title-screen/title_screen.gd
@@ -6,12 +6,34 @@ signal show_level_select()
 signal show_settings_screen()
 signal quit()
 
+# When true, the running intro will be skipped on next frame
+var _skip_requested: bool = false
+
 func _on_start_pressed() -> void:
 	$AnimationPlayer.play("start")
 	$"3D World/enemy_ear/AnimationPlayer".play("Walk")
-	await $AnimationPlayer.animation_finished
+
+	# If a keyboard key is pressed while the animation is running, skip the
+	# remainder of the intro, stop the animations, and start the game immediately.
+	_skip_requested = false
+	while $AnimationPlayer.is_playing():
+		if _skip_requested:
+			# Stop playing the animations so they don't continue after freeing
+			$AnimationPlayer.stop()
+			$"3D World/enemy_ear/AnimationPlayer".stop()
+			break
+		
+		# wait one frame and check again
+		await get_tree().process_frame
+	
+	# Start game
 	start_game.emit()
 	queue_free()
+
+func _unhandled_input(event: InputEvent) -> void:
+	# Handle inputs to skip intro
+	if event is InputEventKey and event.pressed and not event.echo:
+		_skip_requested = true
 
 func _on_credit_pressed() -> void:
 	show_credits.emit()
